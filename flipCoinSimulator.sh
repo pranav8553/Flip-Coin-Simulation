bd2985882fcefaea5c3f28c9821bca0c266f9a93
pranav8553 <pranavCIC065@gmail.com>
2020-08-05 01:26:38 +0530

    Modified:Flipped till either of them won 21 times and showed results

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,8 +4,9 @@
 headCount=0
 tailCount=0
 
+#Flip a coin till either Heads or Tails won 21 times
 function flipACoin(){
-	for (( flip=1; flip<=$times; flip++ ))
+	while [ $headCount -lt 21 ] || [ $tailCount -lt 21 ]
 	do
 		isHead=1
 		randomCheck=$((RANDOM%2))
@@ -16,9 +17,34 @@ function flipACoin(){
 			((tailCount++))
 		fi
 	done
+}
+
+#Calculate difference of times
+function calculateDifference(){
+	winner=$1
+	loser=$2
+	diff=$(($winner - $loser))
+	echo $diff
+}
+
+#To show the winning status
+function winningResults(){
+	if [ $headCount -gt $tailCount ]
+	then
+		difference="$( calculateDifference $headCount $tailCount )"
+		echo "HEADS Won by $difference times!"
+	elif [ $tailCount -gt $headCount ]
+	then
+		difference="$( calculateDifference $tailCount $headCount )"
+		echo "TAILS Won by $difference times!"
+	else
+		echo "Its a Tie!"
+	fi
 	echo "Head: $headCount times"
 	echo "Tail: $tailCount times"
 }
 
-read -p "Enter a number of times you want to flip a coin: " times
-flipACoin $times
+#Main
+flipACoin
+winningResults
+
