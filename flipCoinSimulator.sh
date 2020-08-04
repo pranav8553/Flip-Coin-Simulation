ff094ff352f126764d048fc731cdf7b988903ffa
pranav8553 <pranavCIC065@gmail.com>
2020-08-04 20:51:36 +0530

    Added: Loop through multiple times a show number of times who won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,24 @@
 #!/bin/bash -x
 
-isHead=1
-randomCheck=$((RANDOM%2))
-if [ $randomCheck -eq $isHead ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+#Variables
+headCount=0
+tailCount=0
+
+function flipACoin(){
+	for (( flip=1; flip<=$times; flip++ ))
+	do
+		isHead=1
+		randomCheck=$((RANDOM%2))
+		if [ $randomCheck -eq $isHead ]
+		then
+			((headCount++))
+		else
+			((tailCount++))
+		fi
+	done
+	echo "Head: $headCount times"
+	echo "Tail: $tailCount times"
+}
+
+read -p "Enter a number of times you want to flip a coin: " times
+flipACoin $times
